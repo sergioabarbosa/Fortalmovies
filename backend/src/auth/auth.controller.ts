0fc3158566d4f12d7efdc69e44a18d426267e325
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2023-07-13 15:34:17 -0300

    try correct rout in frontend

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,10 +6,12 @@ import {
   HttpStatus,
   Post,
   Request,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import { LoginDto } from './login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,8 +19,13 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  async login(@Body() credentials: LoginDto, @Res() response) {
+    const token = await this.authService.signIn(
+      credentials.username,
+      credentials.password,
+    );
+    response.cookie('token', token, { httpOnly: true });
+    response.send({ token });
   }
 
   @UseGuards(AuthGuard)
---
+++ backend/src/auth/login.entity.ts
@@ -0,0 +1,16 @@
+// login.entyty.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type LoginDocument = Login & Document;
+
+@Schema()
+export class Login {
+  @Prop()
+  username: string;
+
+  @Prop()
+  password: string;
+}
+
+export const LoginSchema = SchemaFactory.createForClass(Login);
